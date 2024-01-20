0d50fc564ba9015e8a41f96aa98b66fe50206a31
Obili69 <christian.magnus@obrecht.ch>
2024-01-20 17:54:23 +0100

    Lib tests

--- src/AD5593R.h
+++ src/AD5593R.h
@@ -1,5 +1,11 @@
 #ifndef _AD5593R_H_
 #define _AD5593R_H_
 #include <Arduino.h>
-uint8_t AD5593R_Init(uint8_t addr);
+void initADAC(int dacAd, bool gain, bool VREF);
+void setDACpin(unsigned char pin);
+void setADCpin(unsigned char pin);
+void setGPIpin(unsigned char pin);
+void setGPOpin(unsigned char pin);
+void setDACVal(uint8_t DACnum, uint16_t DACvalue);
+uint16_t readADC(uint8_t DACnum);
 #endif
