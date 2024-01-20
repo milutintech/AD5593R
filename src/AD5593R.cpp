3a01802a9024c41cf236750743d72663ad071e9c
Obili69 <christian.magnus@obrecht.ch>
2024-01-20 17:20:30 +0100

    Try

---
+++ src/AD5593R.cpp
@@ -0,0 +1,6 @@
+#include "AD5593R.h"
+#include <Arduino.h>
+
+uint8_t AD5593R_Init(uint8_t addr){
+    return addr;
+}
---
+++ src/AD5593R.h
@@ -0,0 +1,5 @@
+#ifndef _AD5593R_H_
+#derine _AD5593R_H_
+
+uint8_t AD5593R_Init(uint8_t addr);
+#endif
